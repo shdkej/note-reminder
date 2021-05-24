37ff307223cf84feb6477eb8398d482a7d564d34
shdkej <shdkej@github.com>
2021-05-24 15:57:44 +0900

    feat: add build zip script

---
+++ build_zip.sh
@@ -0,0 +1,10 @@
+mkdir build
+pip install -t build -r recommender/requirments.txt
+cd build
+rm -r *.dist-info __pycache__
+find . -name "__pycache__" -type d -exec rm -rf {} +
+rm -rf numpy* bin dateutil threadpoolctl.py six.py scipy*
+zip -r ../event_handling/recommand.zip .
+cd ../event_handling
+zip -r recommand.zip recommender_system.py
+cd ../
